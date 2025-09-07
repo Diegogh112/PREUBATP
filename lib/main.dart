0f0a028bb856bc035b4ca981a8ab72fd845b9627
diego <diegoarmandogh10@gmail.com>
2025-09-06 23:04:35 -0500

    fd

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Prueba Flutter',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mi primera app"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("¡Funciona en mi celular! 🎉")),
+            );
+          },
+          child: const Text("Presióname"),
+        ),
+      ),
+    );
+  }
+}
